9462904ecd985f50f6eceff5d56e962e83f0b088
Carlos1729 <71984114+Carlos1729@users.noreply.github.com>
2021-08-28 16:35:56 +0530

    Update LCS_bottom_up_dp.cpp

--- LCS_bottom_up_dp.cpp
+++ LCS_bottom_up_dp.cpp
@@ -1,29 +1,81 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int LCS(string X, string Y, int n, int m) {
-	int dp[n + 1][m + 1]; // DP - matrix
-
-	// base case of recursion --> for initialization of dp - matrix
-	for (int i = 0; i <= n; i++)
-		for (int j = 0; j <= m; j++)
-			if (i == 0 || j == 0)
-				dp[i][j] = 0;
-
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
-			if (X[i - 1] == Y[j - 1]) // when last character is same
-				dp[i][j] = 1 + dp[i - 1][j - 1];
-			else // when last character is not same -> pick max
-				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-
-	return dp[n][m];
-}
-
-signed main() {
-	string X, Y; cin >> X >> Y;
-	int n = X.length(), m = Y.length();
-
-	cout << LCS(X, Y, n, m) << endl;
-	return 0;
-}
+class Solution {
+public:
+    
+//     int lcs(string t1,string t2,int m,int n)
+//     {
+//         if(m == 0 || n == 0)
+//             return dp[m][n] = 0;
+        
+        
+//         if(dp[m][n] != -1)
+//             return dp[m][n];
+        
+//         if(t1[m-1] == t2[n-1])
+//         {
+//             return dp[m][n] = 1+lcs(t1,t2,m-1,n-1);
+//         }
+        
+//         else 
+//         {
+//             return dp[m][n] = max(lcs(t1,t2,m,n-1),lcs(t1,t2,m-1,n));
+//         }
+//     }
+    
+    int longestCommonSubsequence(string t1, string t2) {
+        
+        
+        //Subse1uences of string 1 and all subsequences of string 2 then basically maintain a map of both of them and find ou the common longer one
+        //O(2^n*l) time
+        
+//         Recursion — Big O(2^N)
+// Memoization — O(N) — time, O(N) — space
+// Bottoms Up — O(N) — time, O(1) — space
+        
+//                        lcs("AXYT", "AYZX")
+//                        /                 \
+//          lcs("AXY", "AYZX")            lcs("AXYT", "AYZ")
+//          /                              /               
+// lcs("AX", "AYZX") lcs("AXY", "AYZ")   lcs("AXY", "AYZ") lcs("AXYT", "AY")
+            
+            
+        
+//         We can clearly see that we are recomputing there lcs between AXY and AYZ we can store this and use it only once instead 
+        
+//         a b c d e     - max of these both the strings 
+//         a c d 
+        
+        
+        int m = t1.length();
+        int n = t2.length();
+        
+        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+        
+        for(int i = 0;i <= m;i++)
+        {
+            for(int j = 0;j <= n;j++)
+            {
+                if(i == 0)
+                    dp[i][0] = 0;
+                else if(j == 0)
+                    dp[0][j] = 0;
+                else if(t1[i-1] == t2[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else 
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        // return dp[m][n];
+        
+        
+        
+        return dp[m][n];
+        
+        //tc - o(n^2)
+        //sc - o(n^2)
+    }
+};
